0795f12d5a8002ee128252d6173bed7de3d5dfa1
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-18 16:07:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -38,7 +38,15 @@ def game_hash
         slam_dunks: 15
       },
       {
-        
+        player_name: "Mason Plumlee",
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 1
       }
       ]
   },
